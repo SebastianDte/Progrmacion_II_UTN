09dbd7a763cf0f71bb0a92d98401abb48ed4a1bc
sebastianDte <sebateunmate@outlook.com>
2025-04-23 23:37:29 -0300

    Ejercicio05-Avance

--- 01.FUNCIONES Y ARRAYS/Ejercicio05.cpp
+++ 01.FUNCIONES Y ARRAYS/Ejercicio05.cpp
@@ -20,9 +20,11 @@ using namespace std;
 // ¿Pegar en el álbum? (S/N)
 
 // No hay límites en la cantidad de paquetes que el coleccionista pueda comprar. La carga de datos finaliza con un número de paquete igual a 0. Calcular e informar:
-// La cantidad de figuritas comunes repetidas.
+// La cantidad de figuritas comunes repetidas. 
 // La cantidad de figuritas faltantes del álbum.
 // La cantidad de hologramas entre las figuritas compradas (sin contar las repetidas).
+
+
 // El estado del álbum pudiendo ser uno de los siguientes casos:
 // "Incompleto" si no se llegaron a pegar las 50 figuritas.
 // "Completo" si se llegaron a pegar las 50 figuritas entre comunes y hologramas.
@@ -37,38 +39,83 @@ using namespace std;
 // Las figuritas que el coleccionista no pega en el álbum son parte de su colección en una pila aparte. Si el coleccionista tiene la figurita #1 varias veces pero nunca la pegó en el álbum entonces el álbum no puede considerarse completo.
 // Se considera una figurita repetida si tiene la misma figurita del mismo tipo más de una vez. Por ejemplo, si tengo la figurita #1 común y #1 holograma en mi colección, no la tengo repetida. Sin embargo, si tres veces tengo la figura #1 holograma en mi colección entonces la tengo repetida DOS veces.
 
+void pegarFigurita(int numeroFigurita, int indexAuxiliar, int album[][2], int &contFigComunesRepetida,int &cantidadHologramas){
+   // La cantidad de hologramas entre las figuritas compradas (sin contar las repetidas).
+  
+   // Verificar si la figurita ya está pegada en el álbum
+   if(indexAuxiliar == 0 && album[numeroFigurita-1][0] == 0){
+      album[numeroFigurita-1][0] = numeroFigurita; // Marca la figurita común como pegada
+   }else if(indexAuxiliar == 1 && album[numeroFigurita-1][1] == 0){
+      album[numeroFigurita-1][1] = numeroFigurita; // Marca la figurita holograma como pegada
+      cantidadHologramas++; // Incrementa el contador de hologramas
+   }else{
+      contFigComunesRepetida++; // Incrementa el contador de figuritas repetidas
+   }
+}
+
+int contarFiguritasFaltantes(int album[][2], int filas, int columnas){
+   int contFiguritasFaltantes = 0;
+   for (int i = 0; i < filas; i++){
+      if(album[i][0] == 0 ){
+         contFiguritasFaltantes++; 
+      }
+      if( album[i][1] == 0){
+         contFiguritasFaltantes++; 
+      }
+      
+   }
+   return contFiguritasFaltantes;
+}
+
 int main()
 {
    setlocale(LC_ALL, "es_ES.UTF-8");
    
+   const int filas = 50; 
+   const int columnas = 2; 
+
    //Declaración de cariables
-   int album[50] = {0}; // Inicializa el álbum con 50 espacios en blanco
-   int paquete[5] = {0}; 
-   char tipo[5] = {' '}; // Tipo de figurita (C o H)
-   bool pegar[5] = {false}; // ¿Pegar en el álbum? (S/N)
-   float costoPaquete = 500.0; // Costo de cada paquete
-   float costoComunes = 20.0; // Costo de cada figurita común
-   float costoHologramas = 50.0; // Costo de cada figurita holograma
-   int comunesRepetidas = 0; // Contador de figuritas comunes repetidas
-   int hologramasRepetidas = 0; // Contador de hologramas
-   int numeroFigurita=0;
-   while(true){
-      cout << "Ingrese el número de paquete (0 para salir): ";
-      int numPaquete;
-      cin >> numPaquete;
-      if(numPaquete == 0) break; // Salir si el número de paquete es 0
-      for(int x = 0; x<5; x++){
+   int album[filas][columnas] = {0}; 
+   int contFigComunesRepetidas = 0;
+   int indexAuxiliar=0;
+   int numeroPaquete = 0;
+   int numeroFigurita = 0;
+   char tipoFigurita = ' ';
+   char respuesta;
+   int cantidadHologramas = 0;
+ 
+   while (true)
+   {
+      
+      for (int i = 0; i < 5; i++)
+      {
+         cout << "Ingrese el número de paquete (0 para salir): ";
+         cin >> numeroPaquete;  
          cout << "Ingrese el número de figurita (1 a 50): ";
          cin >> numeroFigurita;
-         if(numeroFigurita >= 1 && numeroFigurita <= 50){
-            paquete[x] = numeroFigurita; // Guardar el número de figurita
-         }else{
-            cout << "Número de figurita inválido. Debe estar entre 1 y 50." << endl;
-            x--; // Volver a pedir el número de figurita
+         cout << "Ingrese el tipo de figurita (C o H): ";
+         cin >> tipoFigurita;
+
+         if(tipoFigurita == 'C' || tipoFigurita == 'c'){
+            indexAuxiliar = 0; // Común
+         }else if(tipoFigurita == 'H' || tipoFigurita == 'h'){
+            indexAuxiliar = 1; // Holograma
          }
-         
+
+         cout << "¿Pegar en el álbum? (S/N): ";
+         cin >> respuesta;
+         if(respuesta == 'S' || respuesta == 's'){
+            pegarFigurita(numeroFigurita, indexAuxiliar, album, contFigComunesRepetidas,cantidadHologramas);
+         }
       }
    }
+
+   //Cantidad de figuritas comunes repetidas
+   cout << "Cantidad de figuritas comunes repetidas: " << contFigComunesRepetidas << endl;
+
+   // Calcular la cantidad de figuritas faltantes
+   int contFiguritasFaltantes = contarFiguritasFaltantes(album, filas, columnas);
+   cout << "Cantidad de figuritas faltantes: " << contFiguritasFaltantes << endl;
 
    return 0;
 }
