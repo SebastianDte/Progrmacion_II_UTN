11ce69cf9183fcd7ad0cee755f93d1fd97aa1ba7
SebatianDte <sebateumate@outlook.com>
2025-04-26 04:00:48 -0300

    ejercicio02 terminado

--- 02.MEMORIA DINAMICA/Ejercicio_02.cpp
+++ 02.MEMORIA DINAMICA/Ejercicio_02.cpp
@@ -8,8 +8,61 @@ using namespace std;
 // 3- Salir: Sale del programa (no olvidar liberar la memoria)
 // Pista: Recordar que se puede inicializar un puntero con el valor nullptr
 
-int main() {
-    setlocale(LC_ALL, ""); // Configuración de idioma
-    
+void cargarVector(int *&vector, int &cantidad)
+{
+    cout << "Ingrese la cantidad de elementos: ";
+    cin >> cantidad;
+    vector = new int[cantidad];
+    for (int i = 0; i < cantidad; i++)
+    {
+        cout << "Ingrese el elemento " << i + 1 << ": ";
+        cin >> vector[i];
+    }
+}
+
+void mostrarVector(int *vector, int cantidad)
+{
+    cout << "Los elementos del vector son: ";
+    for (int i = 0; i < cantidad; i++)
+    {
+        cout << vector[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "es_ES.UTF-8");
+    int *vector = nullptr;
+    int cantidad = 0;
+    int opcion = 0;
+    while (true)
+    {
+        cout << "Ingrese una opción (1-3): " << endl;
+        cout << "1- Cargar Vector" << endl;
+        cout << "2- Mostrar Vector" << endl;
+        cout << "3- Salir" << endl;
+        cout << "Opción: ";
+        cin >> opcion;
+        switch (opcion)
+        {
+        case 1:
+            cargarVector(vector, cantidad);
+            break;
+        case 2:
+            mostrarVector(vector, cantidad);
+            break;
+        case 3:
+            delete[] vector;
+            vector = nullptr;
+            return 0;
+            break;
+
+        default:
+            cout << "Ingrese una opción válida (1-3): ";
+            break;
+        }
+    }
+
     return 0;
 }
